a274351bdd412232b037a925b79c98a9574fce3a
Mandy Tsang <mandy.tsang007@gmail.com>
2022-01-30 17:03:02 -0800

    set up uuid for newNote

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-
+// 
+const uuid = require("uuid");
 
 // Obtain existing notes
 app.get("/notes", (req, res) => {
@@ -14,10 +15,29 @@ app.get("/notes", (req, res) => {
 })
 
 //create new notes
+app.post("/notes", (req, res) => {
+    // Assign newNote in req.body and give newNote an unique id
+    const newNote = req.body;
+    newNote.id = uuid;
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            res.json(notes);
+        }
+    })
+})
 
-
+//POST /api/notes should receive a new note to save on the request 
+//body, add it to the db.json file, and then return the new note to 
+//the client. You'll need to find a way to give each note a unique id 
+//when it's saved (look into npm packages that could do this for you).
 
 
 
 
 //delete existing notes
+//DELETE /api/notes/:id should receive a query parameter that contains the id 
+//of a note to delete. To delete a note, you'll need to read all notes from 
+//the db.json file, remove the note with the given id property, and then rewrite 
+//the notes to the db.json file.
